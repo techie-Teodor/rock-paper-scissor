dd66c4cc4a90452205877912dcbcac83e108c3b6
Tolbaru Teodor-Iulian <teodor.iul04@gmail.com>
2022-09-20 18:50:53 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultsText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`
+    computerText.textContent = `Computer: ${computer}`
+    resultsText.textContent = checkWinner();
+}));
+
+function computerTurn() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    switch(randomNumber) {
+        case 1: computer = "Rock";
+        break;
+        case 2: computer = "Paper";
+        break;
+        case 3: computer = "Scissors";
+        break;
+    }
+}
+
+function checkWinner() {
+    if (player == computer) {
+        return "It's a tie!";
+    } else if (computer == "Rock") {
+        return (player == "Paper") ? "You win!" : "You lose!"
+    }
+    else if (computer == "Paper") {
+        return (player == "Scissors") ? "You win!" : "You lose!"
+    } else if (computer == "Scissors") {
+        return (player == "Rock") ? "You win!" : "You lose!"
+    }
+}
